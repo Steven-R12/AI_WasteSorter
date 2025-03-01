4a059b2377fc47bbfc72fed7d7d7b03c45252846
Steven-R12 <161642015+Steven-R12@users.noreply.github.com>
2025-03-01 16:58:26 -0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,16 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+def get_model(num_classes=3):
+    model = models.mobilenet_v2(pretrained=True)
+    
+    # Freeze feature extractor layers
+    for param in model.features.parameters():
+        param.requires_grad = False
+    
+    # Modify the classifier
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
+    
+    return model
 
