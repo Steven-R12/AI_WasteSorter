65c38dc87254e84937ceb577531bec4f4986bbaa
Steven-R12 <161642015+Steven-R12@users.noreply.github.com>
2025-04-28 11:25:20 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,16 +1,37 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torch.quantization import quantize_dynamic
 
-def get_model(num_classes=3):
-    model = models.mobilenet_v2(pretrained=True)
+class WasteClassifier(nn.Module):
+    def __init__(self, num_classes=3):
+        super(WasteClassifier, self).__init__()
+        # Using MobileNetV3 for better efficiency
+        self.model = models.mobilenet_v3_small(pretrained=True)
+        
+        # Freeze all layers initially
+        for param in self.model.parameters():
+            param.requires_grad = False
+            
+        # Replace classifier
+        in_features = self.model.classifier[-1].in_features
+        self.model.classifier[-1] = nn.Linear(in_features, num_classes)
+        
+        # Unfreeze last few layers for fine-tuning
+        for param in self.model.features[-4:].parameters():
+            param.requires_grad = True
+        for param in self.model.classifier.parameters():
+            param.requires_grad = True
     
-    # Freeze feature extractor layers
-    for param in model.features.parameters():
-        param.requires_grad = False
+    def forward(self, x):
+        return self.model(x)
     
-    # Modify the classifier
-    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
-    
+    def quantize(self):
+        # Dynamic quantization for Raspberry Pi deployment
+        return quantize_dynamic(self, {nn.Linear}, dtype=torch.qint8)
+
+def get_model(num_classes=3, quantized=False):
+    model = WasteClassifier(num_classes=num_classes)
+    if quantized:
+        model = model.quantize()
     return model
-
